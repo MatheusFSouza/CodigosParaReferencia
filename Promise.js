029fcbfa05cf2141404436a86129351060ad94a5
MatheusFSouza <51373034+MatheusFSouza@users.noreply.github.com>
2022-08-05 09:37:34 -0300

    Create Promise.js

---
+++ Promise.js
@@ -0,0 +1,9 @@
+async function NOMEFUNCAO(){
+
+  let myPromiseVar = new Promise(function (resolve, reject){
+      //função com resolve ou reject
+      resolve(//faz...);
+  });
+  await myPromiseVar
+
+}
